7e99c18a72b82241206bf633df369f450e80f8ed
joshua@janrain.com <joshua@janrain.com>
2014-07-25 15:47:55 -0700

    memory optimization

--- grab_img.py
+++ grab_img.py
@@ -5,13 +5,18 @@ import time
 def snap(number_of_frames):
     cap = cv2.VideoCapture(0)
 
+    frame_buffer = []
+
     for i in range(number_of_frames):
         ret, frame = cap.read()
         width = np.size(frame, 0)
         height = np.size(frame, 1)
         text_color = (0,0,255)
-        cv2.putText(frame, str(time.time()), (width/2, height/2), cv2.FONT_HERSHEY_PLAIN, 1.0, text_color, thickness=1, lineType=cv2.CV_AA)
-        cv2.imwrite('./images/out-%s.png' % str(i), frame)
+        cv2.putText(frame, str(time.time()), (width/2, height/2), cv2.FONT_HERSHEY_PLAIN, 1.0, text_color)
+        frame_buffer.append(frame)
+
+    for f in frame_buffer:
+        cv2.imwrite('./images/out-%s.png' % str(i), f)
 
     cap.release()
     cv2.destroyAllWindows()
