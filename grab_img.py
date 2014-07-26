a80cffc8c300115f30eba733d9d8e1ad75142199
josh-willhite <jwillhite@gmail.com>
2014-07-26 13:30:23 -0700

    figured out how to select a region of interest using numpy array

--- grab_img.py
+++ grab_img.py
@@ -3,22 +3,29 @@ import cv2
 import time
 import subprocess
 
+threshold = .4
+
 def alarm():
     subprocess.call(["beep"])
+
 
 def diff(c, b, a):
     d1 = cv2.absdiff(a, b)
     d2 = cv2.absdiff(b, c)
     sum = cv2.bitwise_and(d1, d2)
-    if cv2.mean(sum)[0] > 1.25:
-        alarm()
-        print cv2.mean(sum)
-
     return sum
 
 
+def getROI(frame):
+    x0 = 100
+    y0 = 100
+    x1 = 500
+    y1 = 500
+    return frame[y0:y1, x0:x1]
+
+
 def snap(number_of_frames):
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
 
     a = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY)
     b = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY)
@@ -38,7 +45,11 @@ def snap(number_of_frames):
         #text_color = (0,0,255)
         #cv2.putText(frame, str(time.time()), (width/2, height/2), cv2.FONT_HERSHEY_PLAIN, 1.0, text_color)
         #cv2.imshow('frame', a)
-        cv2.imshow('another frame', diff(c,b,a))
+        d = diff(c,b,a)
+        cv2.imshow('diff', d)
+        cv2.imshow('roi', getROI(a))
+        if cv2.mean(d)[0] > threshold:
+            cv2.imshow('movement', a)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         #frame_buffer.append(frame)
